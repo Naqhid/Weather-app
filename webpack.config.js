206383aaa6ca8148952197ba3f5e5cc276cd55f7
Naqhid <mnaqhid@gmail.com>
2021-06-15 19:45:19 +0530

    solve-error

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,46 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const path = require('path');
+/* eslint-disable no-undef */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Weather app',
-      template: './src/index.html',
-    }),
-    new Dotenv(),
-  ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Weather app",
+			template: "./src/index.html",
+		}),
+		new Dotenv(),
+	],
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: 'imgs',
-          },
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[hash].[ext]",
+						outputPath: "imgs",
+					},
+				},
+			},
+		],
+	},
 
-  devServer: {
-    watchContentBase: true,
-    contentBase: path.resolve(__dirname, 'dist'),
-    open: true,
-  },
+	devServer: {
+		watchContentBase: true,
+		contentBase: path.resolve(__dirname, "dist"),
+		open: true,
+	},
 };
